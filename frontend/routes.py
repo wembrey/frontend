8dea335a8c4212f672814c98114d244afcd68102
Will Embrey <WEmbrey@WEMBREY-LTQZG.epgpdom.com>
2019-01-01 16:34:49 +0000

    added frontend directory with init

---
+++ frontend/routes.py
@@ -0,0 +1,6 @@
+from frontend import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
--- web.py
+++ web.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return "<html></head><title>Test Page</title></head><body><h1>Welcome!</h1></body</html>"
-
-
-
-
-
-
-
-
-
+from frontend import app
 
 
 if __name__=="__main__":
