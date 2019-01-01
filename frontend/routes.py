332223e3b4348ce151fb82c7e36020197083932e
Will Embrey <WEmbrey@WEMBREY-LTQZG.epgpdom.com>
2019-01-01 17:09:36 +0000

    added templates

--- frontend/routes.py
+++ frontend/routes.py
@@ -1,6 +1,9 @@
 from frontend import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
+    demo = {'demo': 'Sec Ops'}
     return "Hello, World!"
+    return render_template('index.html', title='Home', demo=demo)
